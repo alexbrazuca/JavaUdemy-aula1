2f7477127b62afe9e3426bf7a4a0b8b73d8de4ea
Alex Silva <alexbrazuca@live.com>
2020-03-29 13:46:38 -0300

    Adcione um ArrayList de perguntas para serem chamadas no jogo

---
+++ src/Application/Perguntas.java
@@ -0,0 +1,16 @@
+package Application;
+
+import java.util.ArrayList;
+
+public class Perguntas {
+
+	public  ArrayList perguntas = new ArrayList();
+	
+	public void perguntas() {
+		perguntas.add("Qual o comando para criar uma arvore de diretorios");
+		perguntas.add("Qual comando para  copiar arquivos e os atrubutos originais");
+		perguntas.add("O que faz o comando PR");
+		perguntas.add("O que faz o comando SORT");
+		perguntas.add("O que faz o comando cat e tac");
+	}
+}
